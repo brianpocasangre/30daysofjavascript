cb31ad4fa33a36aca628d19aeecb47deb1a0ff3c
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-17 15:54:17 -0700

    work with SpeechRecognition

--- speechrecognition/scripts.js
+++ speechrecognition/scripts.js
@@ -1,5 +1,28 @@
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognitoin();
+const recognition = new SpeechRecognition();
+// allows to populate as you speak instead of populating until you stop speaking
 recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+  if (transcript.includes('unicorn')) console.log(transcript);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
